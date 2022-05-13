3a3a9b835c00db3e89462441256a8b5d13c6c950
Papatricio-UNO <patparonable@gmail.com>
2022-05-12 21:55:42 -0500

    Altered NeuralNine_Coding2.py with coding from Kite's coding.

--- Final_Project_Two_(CUT)/NeuralNine_Coding2.py
+++ Final_Project_Two_(CUT)/NeuralNine_Coding2.py
@@ -1,19 +1,22 @@
 # Hangman game based on NeuralNine's video: https://youtu.be/5x6iAKdJB6U
+# with improvements from Kite's videos:  https://youtu.be/m4nEnsavl6w
 # Version with random word selection
-# Ideas for tweaking: Add error exceptions for integers, words, etc & retry option
+# Additions: A retry option with
+# new words after finishing game
 
 import random
-
-with open('wordlist.txt', 'r') as f:
-    words = f.readlines()
-
-word = random.choice(words)[:-1]
+from hangman_words import word_selection
 
 
-def main():
-    allowed_errors = 14
+def get_word():
+    word = random.choice(word_selection)
+    return word
+
+
+def play(word):
+    allowed_errors = 7
     guesses = []
-    done = False
+    done: bool = False
 
     while not done:
         for letter in word:
@@ -22,9 +25,10 @@ def main():
             else:
                 print("_", end=" ")
         print("")
-        done = True
 
-        guess = input(f"Allowed Errors Left {allowed_errors}, Next Guess: ")
+        guess = input(f"Let's Guess That Word! \n"
+                      f"Allowed Errors: {allowed_errors} \n"
+                      f"Guess a letter to find that word: ")
         guesses.append(guess.lower())
         if guess.lower() not in word.lower():
             allowed_errors -= 1
@@ -35,11 +39,18 @@ def main():
         for letter in word:
             if letter.lower() not in guesses:
                 done = False
-
     if done:
         print(f"You found the word! The word was {word}!")
     else:
         print(f"Game Over! The word was {word}.")
+
+
+def main():
+    word = get_word()
+    play(word)
+    while input("Play Again? (Y/N) ").upper() == "Y":
+        word = get_word()
+        play(word)
 
 
 if __name__ == "__main__":
